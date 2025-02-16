cda3e52a70fe749357551b1f79165433aab05f73
Tom <github@tom-ac.dev>
2025-02-16 19:55:14 +0000

    gh-xx: Add `__slots__` to `AnnouncementMethod`

--- domprob/announcements/method.py
+++ domprob/announcements/method.py
@@ -487,6 +487,8 @@ class AnnouncementMethod(BaseAnnouncementMethod, Generic[_PMeth, _RMeth]):
         AnnouncementMethod(meth=<function Foo.bar at 0x...>)
     """
 
+    __slots__: list[str] = ['_meth', '_supp_instrums', '_binder']
+
     def __init__(
         self,
         meth: Callable[_PMeth, _RMeth],
